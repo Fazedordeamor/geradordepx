619da3297011bc061081a657b5ab49eb0238f009
[dyad] <grupojcmphone@gmail.com>
2025-11-27 17:34:57 -0300

    Reverted all changes back to version 37f4bc00220af4165246ea606f498c1c76a9eeac

--- src/types/qrcode.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "qrcode" {
-  const QRCode: {
-    toDataURL(text: string, options?: any): Promise<string>;
-    toString?(text: string, options?: any): Promise<string>;
-  };
-
-  export default QRCode;
-}
